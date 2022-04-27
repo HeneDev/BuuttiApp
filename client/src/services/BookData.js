9d4656bebebea02d299100763d42b9aef148283d
HeneDev <HenriPKemppainen@outlook.com>
2022-04-27 19:01:02 +0300

    Changed baseurl to a secret key, removed default files, added assets

--- client/src/services/BookData.js
+++ client/src/services/BookData.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = "http://localhost:3001/api/books"
+const baseUrl = process.env.REACT_APP_BASE_URL
 
 const retrieveAllBooks = async() => {
     const response = await axios.get(baseUrl)
